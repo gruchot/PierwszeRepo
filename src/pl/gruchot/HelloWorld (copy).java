cd98afcacf73e7c1bc563bf83c0a8daf37c3f4bd
Michał Gruchota <gruchot@gmail.com>
2018-03-10 11:25:36 +0100

    "usunięcie pliku"

--- src/pl/gruchot/HelloWorld (copy).java
+++
@@ -1,9 +0,0 @@
-package pl.gruchot;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Wazzaaa");
-
-        System.out.println("Hello World");
-    }
-}
