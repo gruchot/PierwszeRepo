2b49f2a5fa13fcf47f86cdb8105e25dd25033d13
Michał Gruchota <gruchot@gmail.com>
2018-03-10 10:30:24 +0100

    Dodanie nowej linii

--- src/pl/gruchot/HelloWorld (copy).java
+++ src/pl/gruchot/HelloWorld (copy).java
@@ -2,6 +2,8 @@ package pl.gruchot;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        System.out.println("Wazzaaa");
+
         System.out.println("Hello World");
     }
 }
